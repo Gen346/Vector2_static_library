37e6a543502ea925ddc40fb288d842a68da1f21c
Gen346 <kelzreez@gmail.com>
2025-10-28 20:59:00 +0200

    Added tests in main

--- Vector2/main.cpp
+++ Vector2/main.cpp
@@ -2,5 +2,35 @@
 
 int main()
 {
+	Vector2 testVectorOne = {3.0f, 4.0f};
+	Vector2 testVectorTwo = { 3.0f, 4.0f };
+	Vector2 testVectorThree = { 3.1f, 4.0f };
 
+	if (testVectorOne == testVectorTwo)
+	{
+		std::cout << testVectorOne << " is equal to " << testVectorTwo << "\n";
+	}
+	else
+	{
+		std::cout << testVectorOne << " is not equal to " << testVectorTwo << "\n";
+	}
+	if (testVectorOne == testVectorThree)
+	{
+		std::cout << testVectorOne << " is equal to " << testVectorThree << "\n";
+	}
+	else
+	{
+		std::cout << testVectorOne << " is not equal to " << testVectorThree << "\n";
+	}
+	if (testVectorOne == (double)testVectorThree)
+	{
+		std::cout << testVectorOne << " is equal to " << testVectorThree << "\n";
+	}
+	else
+	{
+		std::cout << testVectorOne << " is not equal to " << testVectorThree << "\n";
+	}
+
+	std::cin.get();
+	return 0;
 }
